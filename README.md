6ec2652072cd1f7d1ead7a87e77d0404f0a69289
Alex Chepura <a.chepura@gmail.com>
2016-03-23 20:25:09 +0300

    preparing for publish

--- README.md
+++ README.md
@@ -1,35 +1,19 @@
 # Redaxtor-codemirror
 Redaxtor-codemirror is a source-editor plugin for Redaxtor library
 
-## Build for example
-```
-npm install
-npm run build:umd:example
-```
-
 ## The Gist (with Redaxtor)
 ```js
-import Redaxtor from 'Redaxtor'
+var Redaxtor = require('redaxtor');
+var RedaxtorCodemirror = require('redaxtor-codemirror');
+require('codemirror/mode/htmlmixed/htmlmixed')
 
-//import your components for the Redaxtor
-import MediumEditor from "./components/MediumEditor/"
-let components = {
-    navigation: Navigation,
-    source: RedaxtorCodemirror
+var components = {
+    html: RedaxtorCodemirror
 }
 
-//initialise Redaxtor with options
-//ANY option and suboption is optional and has default value
 let redaxtor = new Redaxtor({
     pieces: {
-        attribute: "data-piece",
-        idAttribute: "data-id",
-        fetchAttribute: "data-fetch",
-        saveAttribute: "data-save",
-        components: components,
-        initialState: {}
-    },
-    i18n: i18n,
-    state: {}
+        components: components
+    }
 });
 ```
--- src/RedaxtorCodemirror.js
+++ src/RedaxtorCodemirror.js
@@ -1,15 +1,14 @@
 import React, {Component} from "react"
-import Dialog from 'material-ui/lib/dialog';
-import FlatButton from 'material-ui/lib/flat-button';
-import RaisedButton from 'material-ui/lib/raised-button';
-import Codemirror from 'react-codemirror';
-require('codemirror/mode/htmlmixed/htmlmixed');
-require('style!css!codemirror/lib/codemirror.css');
+import Dialog from 'material-ui/lib/dialog'
+import FlatButton from 'material-ui/lib/flat-button'
+import RaisedButton from 'material-ui/lib/raised-button'
+import Codemirror from 'react-codemirror'
+require('codemirror/mode/htmlmixed/htmlmixed')
 
 export default class CodeMirror extends Component {
     constructor(props) {
         super(props);
-        if (this.props.node){
+        if (this.props.node) {
             this.state = {
                 sourceEditorActive: false
             };
@@ -17,41 +16,45 @@ export default class CodeMirror extends Component {
             this.state = {}
         }
     }
-    updateCode (newCode) {
+
+    updateCode(newCode) {
         this.code = newCode
     }
-    onSave(){
+
+    onSave() {
         this.props.updatePiece && this.props.updatePiece(this.props.id, {data: {html: this.code}})
         this.props.savePiece && this.props.savePiece(this.props.id)
         this.setState({sourceEditorActive: false})
     }
-    makeReadable(readableHTML){
-        var lb = '\r\n',i;
-        var htags = ["<html","</html>","</head>","<title","</title>","<meta","<link","<style","</style>","</body>"];
-        for (i=0; i<htags.length; ++i) {
+
+    makeReadable(readableHTML) {
+        var lb = '\r\n', i;
+        var htags = ["<html", "</html>", "</head>", "<title", "</title>", "<meta", "<link", "<style", "</style>", "</body>"];
+        for (i = 0; i < htags.length; ++i) {
             var hhh = htags[i];
-            readableHTML = readableHTML.replace(new RegExp(hhh,'gi'),lb+hhh);
+            readableHTML = readableHTML.replace(new RegExp(hhh, 'gi'), lb + hhh);
         }
-        var btags = ["</div>","</span>","</form>","</fieldset>","<br>","<br />","<hr","<pre","</pre>","<blockquote",
-            "</blockquote>","<ul","</ul>","<ol","</ol>","<li","<dl","</dl>","<dt","</dt>","<dd","</dd>",
-            "<\!--","<table","</table>","<caption","</caption>","<th","</th>","<tr","</tr>","<td","</td>","<script",
-            "</script>","<noscript","</noscript>","<a", "</a>", "<img"];
-        for (i=0; i<btags.length; ++i) {
+        var btags = ["</div>", "</span>", "</form>", "</fieldset>", "<br>", "<br />", "<hr", "<pre", "</pre>", "<blockquote",
+            "</blockquote>", "<ul", "</ul>", "<ol", "</ol>", "<li", "<dl", "</dl>", "<dt", "</dt>", "<dd", "</dd>",
+            "<\!--", "<table", "</table>", "<caption", "</caption>", "<th", "</th>", "<tr", "</tr>", "<td", "</td>", "<script",
+            "</script>", "<noscript", "</noscript>", "<a", "</a>", "<img"];
+        for (i = 0; i < btags.length; ++i) {
             var bbb = btags[i];
-            readableHTML = readableHTML.replace(new RegExp(bbb,'gi'),lb+bbb);
+            readableHTML = readableHTML.replace(new RegExp(bbb, 'gi'), lb + bbb);
         }
-        var ftags = ["<label","</label>","<legend","</legend>","<object","</object>","<embed","</embed>","<select","</select>","<option","<option","<input","<textarea","</textarea>"];
-        for (i=0; i<ftags.length; ++i) {
+        var ftags = ["<label", "</label>", "<legend", "</legend>", "<object", "</object>", "<embed", "</embed>", "<select", "</select>", "<option", "<option", "<input", "<textarea", "</textarea>"];
+        for (i = 0; i < ftags.length; ++i) {
             var fff = ftags[i];
-            readableHTML = readableHTML.replace(new RegExp(fff,'gi'),lb+fff);
+            readableHTML = readableHTML.replace(new RegExp(fff, 'gi'), lb + fff);
         }
-        var xtags = ["<body","<head","<div","<span","<p","<form","<fieldset"];
-        for (i=0; i<xtags.length; ++i) {
+        var xtags = ["<body", "<head", "<div", "<span", "<p", "<form", "<fieldset"];
+        for (i = 0; i < xtags.length; ++i) {
             var xxx = xtags[i];
-            readableHTML = readableHTML.replace(new RegExp(xxx,'gi'),lb+lb+xxx);
+            readableHTML = readableHTML.replace(new RegExp(xxx, 'gi'), lb + lb + xxx);
         }
         return readableHTML;
     }
+
     render() {
 
         const customContentStyle = {
@@ -63,37 +66,29 @@ export default class CodeMirror extends Component {
             mode: 'htmlmixed'
         };
         var codemirror = null;
-        debugger
-        if (!this.state.sourceEditorActive&&this.props.node) {
-            codemirror = <div dangerouslySetInnerHTML={{__html: this.props.data.html}} onClick={e=>{e.preventDefault();this.setState({sourceEditorActive: true})}}></div>;
+
+        if (!this.state.sourceEditorActive && this.props.node) {
+            codemirror = <div dangerouslySetInnerHTML={{__html: this.props.data.html}}
+                              onClick={e=>{e.preventDefault();this.setState({sourceEditorActive: true})}}></div>;
         } else {
             const actions = [
-                <FlatButton
-                    label="Cancel"
-                    secondary={true}
-                    onClick={()=>this.props.node?this.setState({sourceEditorActive: false}):(this.props.onClose && this.props.onClose())}
+                <FlatButton label="Cancel" secondary={true}
+                            onClick={()=>this.props.node?this.setState({sourceEditorActive: false}):(this.props.onClose && this.props.onClose())}
                 />,
-                <FlatButton
-                    label="Save"
-                    primary={true}
-                    keyboardFocused={true}
-                    onClick={()=>this.props.node?this.onSave():(this.props.onSave && this.props.onSave(this.code))}
+                <FlatButton label="Save" primary={true} keyboardFocused={true}
+                            onClick={()=>this.props.node?this.onSave():(this.props.onSave && this.props.onSave(this.code))}
                 />
             ];
-            codemirror = <Dialog
-                title="Dialog With Actions"
-                actions={actions}
-                modal={true}
-                open={true}
-                contentStyle={customContentStyle}
-            >
-                <Codemirror value={this.makeReadable(this.props.node?this.props.data.html:this.props.html)} onChange={this.updateCode.bind(this)} options={options} />
+            codemirror = <Dialog title="Dialog With Actions" actions={actions} modal={true} open={true}
+                                 contentStyle={customContentStyle}>
+                <Codemirror value={this.makeReadable(this.props.node?this.props.data.html:this.props.html)}
+                            onChange={this.updateCode.bind(this)} options={options}/>
             </Dialog>
         }
-        return(
+        return (
             <div>
-            {codemirror}
-                </div>
+                {codemirror}
+            </div>
         )
 
     }
