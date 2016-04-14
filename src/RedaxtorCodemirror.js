37da3118b0d59cb853b01b0226378efe43302e8e
Dmitry <zmicerboksha@gmail.com>
2016-04-14 22:08:06 +0300

    style refactor

--- src/RedaxtorCodemirror.js
+++ src/RedaxtorCodemirror.js
@@ -1,8 +1,7 @@
 import React, {Component} from "react"
-import Dialog from 'material-ui/lib/dialog'
-import FlatButton from 'material-ui/lib/flat-button'
 import Codemirror from 'react-codemirror'
 import {html as html_beautify} from 'js-beautify'
+import Modal from 'react-modal'
 require('codemirror/mode/htmlmixed/htmlmixed');
 
 export default class CodeMirror extends Component {
@@ -30,12 +29,11 @@ export default class CodeMirror extends Component {
         this.setState({sourceEditorActive: false})
     }
 
-    render() {
+    onClose() {
+        this.props.node ? this.setState({sourceEditorActive: false}) : (this.props.onClose && this.props.onClose())
+    }
 
-        const customContentStyle = {
-            width: '80%',
-            maxWidth: 'none'
-        };
+    render() {
         var options = {
             lineNumbers: true,
             mode: 'htmlmixed'
@@ -43,26 +41,24 @@ export default class CodeMirror extends Component {
         var codemirror = null;
 
         if (!this.state.sourceEditorActive && this.props.node) {
-            codemirror = <this.props.wrapper style={this.props.style} dangerouslySetInnerHTML={{__html: this.props.data.html}}
-                              onClick={e=>{e.preventDefault();this.setState({sourceEditorActive: true})}}/>;
+            codemirror = <this.props.wrapper className={this.props.className}
+                                             dangerouslySetInnerHTML={{__html: this.props.data.html}}
+                                             onClick={e=>{e.preventDefault();this.setState({sourceEditorActive: true})}}/>;
         } else {
-            const actions = [
-                <FlatButton label="Cancel" secondary={true}
-                            onClick={()=>this.props.node?this.setState({sourceEditorActive: false}):(this.props.onClose && this.props.onClose())}
-                />,
-                <FlatButton label="Save" primary={true} keyboardFocused={true}
-                            onClick={()=>this.props.node?this.onSave():(this.props.onSave && this.props.onSave(this.code))}
-                />
-            ];
             codemirror =
                 <div>
-                    <div dangerouslySetInnerHTML={{__html: (this.props.data&&this.props.data.html)||this.props.html}}></div>
-                    <Dialog title="Edit Source code here" actions={actions} modal={true} open={true}
-                            contentStyle={customContentStyle}>
+                    <div
+                        dangerouslySetInnerHTML={{__html: (this.props.data&&this.props.data.html)||this.props.html}}></div>
+                    <Modal isOpen={true} overlayClassName="modal-overlay" className="modal-content"
+                           onRequestClose={this.onClose.bind(this)}>
                         <Codemirror
                             value={html_beautify(this.props.node?this.props.data.html:this.props.html,this.beautifyOptions)}
                             onChange={this.updateCode.bind(this)} options={options}/>
-                    </Dialog>
+                        <div className="actions-bar">
+                            <div className="button button-cancel" onClick={this.onClose.bind(this)}>Cancel</div>
+                            <div className="button button-save" onClick={()=>this.props.node?this.onSave():(this.props.onSave && this.props.onSave(this.code))}>Save</div>
+                        </div>
+                    </Modal>
                 </div>
 
 
