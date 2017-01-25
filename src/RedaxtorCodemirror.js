493284ccc7ffe32f928b104d95da9632cf2885bb
Aleksey Alifer <aleksei.alifer@spiralscout.com>
2017-01-25 18:13:13 +0300

    change rendering of codemirror

--- src/RedaxtorCodemirror.js
+++ src/RedaxtorCodemirror.js
@@ -7,6 +7,9 @@ require('codemirror/mode/htmlmixed/htmlmixed');
 export default class CodeMirror extends Component {
     constructor(props) {
         super(props);
+
+        this.onClickBound = this.onClick.bind(this);
+
         this.beautifyOptions = {
             "wrap_line_length": 140
         };
@@ -30,8 +33,8 @@ export default class CodeMirror extends Component {
     }
 
     onSave() {
-        this.props.updatePiece && this.props.updatePiece(this.props.id, {data: {html: this.code}})
-        this.props.savePiece && this.props.savePiece(this.props.id)
+        this.props.updatePiece && this.props.updatePiece(this.props.id, {data: {html: this.code}});
+        this.props.savePiece && this.props.savePiece(this.props.id);
         this.setState({sourceEditorActive: false})
     }
 
@@ -39,49 +42,84 @@ export default class CodeMirror extends Component {
         this.props.node ? this.setState({sourceEditorActive: false}) : (this.props.onClose && this.props.onClose())
     }
 
+    createEditor(){
+        // this.props.node
+        this.props.node.addEventListener('click', this.onClickBound);
+    }
+
+    destroyEditor(){
+        this.props.node.removeEventListener('click', this.onClickBound);
+    }
+
+    renderNonReactAttributes(){
+        if (this.props.editorActive && !this.state.sourceEditorActive) {
+            this.createEditor();
+            this.props.node.classList.add(...this.props.className.split(' '));
+        }
+        else {
+            this.destroyEditor();
+            this.props.node.classList.remove(...this.props.className.split(' '));
+        }
+
+    }
+
+    onClick(e){
+        e.preventDefault();
+        this.setState({sourceEditorActive: true});
+    }
+
     render() {
-        var options = {
+       /* var options = {
             lineNumbers: true,
             mode: 'htmlmixed'
         };
         var codemirror = null;
         if (this.props.hasOwnProperty('editorActive') && !this.props.editorActive) {
             codemirror =
-                <this.props.wrapper className={this.props.className}
+                <this.props.node className={this.props.className}
                     dangerouslySetInnerHTML={{__html: (this.props.data && this.props.data.html) || this.props.html}}/>
         }
         else if (!this.state.sourceEditorActive && this.props.node) {
             codemirror =
-                <this.props.wrapper className={this.props.className} style={this.props.style} dangerouslySetInnerHTML={{__html: this.props.data.html}}
+                <this.props.node className={this.props.className} style={this.props.style} dangerouslySetInnerHTML={{__html: this.props.data.html}}
                                     onClick={e=> {
                                         e.preventDefault();
                                         this.setState({sourceEditorActive: true})
                                     }}/>;
         } else {
             codemirror =
-                <this.props.wrapper className={this.props.className}>
+                <this.props.node className={this.props.className}>
                     {this.state.sourceEditorActive && <div
                         dangerouslySetInnerHTML={{__html: (this.props.data && this.props.data.html) || this.props.html}}></div>}
-                    <Modal contentLabel="Edit source" isOpen={true} overlayClassName="r_modal-overlay r_visible"
-                           className="r_modal-content"
-                           onRequestClose={this.onClose.bind(this)}>
-                        <Codemirror
-                            value={html_beautify(this.props.node ? this.props.data.html : this.props.html, this.beautifyOptions)}
-                            onChange={this.updateCode.bind(this)} options={options}/>
-                        <div className="actions-bar">
-                            <div className="button button-cancel" onClick={this.onClose.bind(this)}>Cancel</div>
-                            <div className="button button-save"
-                                 onClick={()=>this.props.node ? this.onSave() : (this.props.onSave && this.props.onSave(this.code))}>
-                                Save
-                            </div>
-                        </div>
-                    </Modal>
-                </this.props.wrapper>
+
+                </this.props.node>
 
 
+        }*/
+        let codemirror = React.createElement(this.props.wrapper, {});
+        if(this.state.sourceEditorActive){
+            let options = {
+                lineNumbers: true,
+                mode: 'htmlmixed'
+            };
+            codemirror =  <Modal contentLabel="Edit source" isOpen={true} overlayClassName="r_modal-overlay r_visible"
+                                 className="r_modal-content"
+                                 onRequestClose={this.onClose.bind(this)}>
+                <Codemirror
+                    value={html_beautify(this.props.node ? this.props.data.html : this.props.html, this.beautifyOptions)}
+                    onChange={this.updateCode.bind(this)} options={options}/>
+                <div className="actions-bar">
+                    <div className="button button-cancel" onClick={this.onClose.bind(this)}>Cancel</div>
+                    <div className="button button-save"
+                         onClick={()=>this.props.node ? this.onSave() : (this.props.onSave && this.props.onSave(this.code))}>
+                        Save
+                    </div>
+                </div>
+            </Modal>;
         }
 
-        return codemirror
+        this.renderNonReactAttributes();
+        return codemirror;
     }
 }
 
@@ -90,5 +128,5 @@ export default class CodeMirror extends Component {
  * Specify component should be rendered inside target node and capture all inside html
  * @type {string}
  */
-CodeMirror.__renderType = "INSIDE";
+CodeMirror.__renderType = "BEFORE";
 CodeMirror.__name = "Source";
